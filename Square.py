620b57d366bab6bdcee73cf4c3cf39b994c0a5fe
Tani-alt <Tanirika.gupta@gmail.com>
2025-02-20 19:53:56 +0530

    colorful

---
+++ Square.py
@@ -0,0 +1,25 @@
+#SQUARE
+import turtle
+turtle.Screen().bgcolor("grey")
+turtle.Screen().setup(200,300)
+polygon=turtle.Turtle()
+polygon.pencolor("red")
+polygon.right(90)
+polygon.forward(90)
+
+polygon.pencolor("orange")
+polygon.right(90)
+polygon.forward(90)
+
+
+polygon.pencolor("yellow")
+polygon.pendown()
+polygon.right(90)
+polygon.forward(90)
+
+polygon.pencolor("green")
+polygon.right(90)
+polygon.forward(90)
+
+
+turtle.done()
